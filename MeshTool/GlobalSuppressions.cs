76c9171dcfe32141c233f2ffc96927704e7a1981
usausa <machi.pon@gmail.com>
2024-11-17 16:05:34 +0900

    Update setting

--- MeshTool/GlobalSuppressions.cs
+++ MeshTool/GlobalSuppressions.cs
@@ -1,1 +1,1 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0320:Make anonymous function static", Justification = "Ignore")]
