ea7dae601e694a3cc6fefeb248bb943461a23b5c
usausa <machi.pon@gmail.com>
2025-12-24 12:04:56 +0900

    Update library

---
+++ MeshTool/Commands.cs
@@ -0,0 +1,102 @@
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable MemberCanBeProtected.Global
+// ReSharper disable MemberCanBePrivate.Global
+namespace MeshTool;
+
+using System.Buffers.Binary;
+
+using Smart.CommandLine.Hosting;
+
+public static class CommandBuilderExtensions
+{
+    public static void AddCommands(this ICommandBuilder commands)
+    {
+        commands.AddCommand<LedCommand>();
+    }
+}
+
+// LED
+[Command("led", Description = "LED")]
+public sealed class LedCommand : ICommandHandler
+{
+    [Option<string>("--address", "-a", Description = "Address", IsRequired = true)]
+    public string Address { get; set; } = default!;
+
+    [Option<string>("--color", "-c", Description = "Address", DefaultValue = "FFFFFF")]
+    public string Color { get; set; } = default!;
+
+    [Option<int>("--time", "-t", Description = "Time", DefaultValue = -1)]
+    public int Time { get; set; } = default!;
+
+    [Option<int>("--on", "-n", Description = "ON", DefaultValue = -1)]
+    public int On { get; set; } = default!;
+
+    [Option<int>("--off", "-f", Description = "OFF", DefaultValue = 0)]
+    public int Off { get; set; } = default!;
+
+    [Option<byte>("--pattern", "-p", Description = "Pattern", DefaultValue = 1)]
+    public byte Pattern { get; set; } = default!;
+
+    public async ValueTask ExecuteAsync(CommandContext context)
+    {
+        var rgb = Convert.ToInt32(Color, 16);
+
+        using var device = await MeshHelper.DiscoverDeviceAsync("MESH-100LE", Address).ConfigureAwait(false);
+        if (device is null)
+        {
+            Console.WriteLine("Device not found.");
+            return;
+        }
+
+        if (!await MeshHelper.PairAsync(device).ConfigureAwait(false))
+        {
+            Console.WriteLine("Pairing failed.");
+            return;
+        }
+
+        var characteristic = await MeshHelper.ResolveWriteCharacteristic(device).ConfigureAwait(false);
+        if (characteristic is null)
+        {
+            Console.WriteLine("Service resolve failed.");
+            return;
+        }
+
+        if (!await MeshHelper.InitializeMeshAsync(characteristic).ConfigureAwait(false))
+        {
+            Console.WriteLine("Initialize mesh failed.");
+            return;
+        }
+
+        var command = new byte[]
+        {
+            0x01,
+            0x00,
+            (byte)((rgb >> 16) & 0xff),
+            0x00,
+            (byte)((rgb >> 8) & 0xff),
+            0x00,
+            (byte)(rgb & 0xff),
+            0x00, 0x00,
+            0x00, 0x00,
+            0x00, 0x00,
+            Pattern,
+            0x00
+        };
+        WriteTime(command.AsSpan(7), Time);
+        WriteTime(command.AsSpan(9), On);
+        WriteTime(command.AsSpan(11), Off);
+        command[^1] = MeshHelper.CalcCrc(command.AsSpan(0, command.Length - 1));
+        if (!await MeshHelper.WriteCommandAsync(characteristic, command).ConfigureAwait(false))
+        {
+            Console.WriteLine("Write command failed.");
+        }
+
+        return;
+
+        static void WriteTime(Span<byte> buffer, int value)
+        {
+            BinaryPrimitives.TryWriteUInt16LittleEndian(buffer, (ushort)(value is < 0 or > 65535 ? 65535 : value));
+        }
+    }
+}
--- MeshTool/Program.cs
+++ MeshTool/Program.cs
@@ -1,82 +1,17 @@
-#pragma warning disable IDE0320
-// ReSharper disable UseObjectOrCollectionInitializer
-
-using System.Buffers.Binary;
-using System.CommandLine;
-using System.CommandLine.NamingConventionBinder;
 using MeshTool;
 
-var rootCommand = new RootCommand("MESH tool");
-rootCommand.AddGlobalOption(new Option<string>(["--address", "-a"], "Address"));
+using Smart.CommandLine.Hosting;
 
-// LED
-var leCommand = new Command("le", "LED");
-leCommand.AddOption(new Option<string>(["--color", "-c"], () => "FFFFFF"));
-leCommand.AddOption(new Option<int>(["--time", "-t"], () => -1));
-leCommand.AddOption(new Option<int>(["--on", "-n"], () => -1));
-leCommand.AddOption(new Option<int>(["--off", "-f"], () => 0));
-leCommand.AddOption(new Option<byte>(["--pattern", "-p"], () => 1));
-leCommand.Handler = CommandHandler.Create(async (string address, string color, int time, int on, int off, byte pattern) =>
+var builder = CommandHost.CreateBuilder(args);
+builder.ConfigureCommands(commands =>
 {
-    var rgb = Convert.ToInt32(color, 16);
-
-    using var device = await MeshHelper.DiscoverDeviceAsync("MESH-100LE", address).ConfigureAwait(false);
-    if (device is null)
+    commands.ConfigureRootCommand(root =>
     {
-        Console.WriteLine("Device not found.");
-        return;
-    }
+        root.WithDescription("MESH tool");
+    });
 
-    if (!await MeshHelper.PairAsync(device).ConfigureAwait(false))
-    {
-        Console.WriteLine("Pairing failed.");
-        return;
-    }
-
-    var characteristic = await MeshHelper.ResolveWriteCharacteristic(device).ConfigureAwait(false);
-    if (characteristic is null)
-    {
-        Console.WriteLine("Service resolve failed.");
-        return;
-    }
-
-    if (!await MeshHelper.InitializeMeshAsync(characteristic).ConfigureAwait(false))
-    {
-        Console.WriteLine("Initialize mesh failed.");
-        return;
-    }
-
-    var command = new byte[]
-    {
-        0x01,
-        0x00,
-        (byte)((rgb >> 16) & 0xff),
-        0x00,
-        (byte)((rgb >> 8) & 0xff),
-        0x00,
-        (byte)(rgb & 0xff),
-        0x00, 0x00,
-        0x00, 0x00,
-        0x00, 0x00,
-        pattern,
-        0x00
-    };
-    WriteTime(command.AsSpan(7), time);
-    WriteTime(command.AsSpan(9), on);
-    WriteTime(command.AsSpan(11), off);
-    command[^1] = MeshHelper.CalcCrc(command.AsSpan(0, command.Length - 1));
-    if (!await MeshHelper.WriteCommandAsync(characteristic, command).ConfigureAwait(false))
-    {
-        Console.WriteLine("Write command failed.");
-    }
-
-    return;
-
-    static void WriteTime(Span<byte> buffer, int value)
-    {
-        BinaryPrimitives.TryWriteUInt16LittleEndian(buffer, (ushort)(value is < 0 or > 65535 ? 65535 : value));
-    }
+    commands.AddCommands();
 });
-rootCommand.Add(leCommand);
 
-return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
+var host = builder.Build();
+return await host.RunAsync();
